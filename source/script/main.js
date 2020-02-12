0e2e3e505346d5545737c16bed770a7fe27df48d
retrun18 <648437254@qq.com>
2020-02-12 10:14:35 +0800

    test

--- source/script/main.js
+++ source/script/main.js
@@ -23,86 +23,87 @@ var map = new ol.Map({
     view: view
 })
 let areasurl='https://geo.datav.aliyun.com/areas/bound/100000_full.json';
-	var style = new ol.style.Style({
-  fill: new ol.style.Fill({
-    color: 'rgba(255, 155, 0, 0.6)'
-  }),
-  stroke: new ol.style.Stroke({
-    color: '#319FD3',
-    width: 1
-  }),
-  text: new ol.style.Text({
-    font: '12px Calibri,sans-serif',
-    fill: new ol.style.Fill({
-      color: '#000'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#fff',
-      width: 3
-    })
-  })
-});
-	
-var vectorLayer1 = new ol.layer.Vector({
-  source: new ol.source.Vector({                                                url: areasurl,
-    format: new ol.format.GeoJSON()                                           }),                                                                         style: function(feature) {
-    style.getText().setText(feature.get('name'));
-    return style;                                                               }                                                                       });
-	//map.addLayer(vectorLayer1)
-	fetch(areasurl,{method:"GET",mode:"cors"}).then(function(response){
-	return response.json();
-}).then(function(json){
-	getyiqing().then((yqjson)=>{
-		console.log(yqjson)
+let yqurl2="https://tianqiapi.com/api?version=epidemic&appid=22747463&appsecret=6uKNYwJa"
+	fetch(areasurl,{method:'get',mode:'cors',headers:{'Content-Type':"application/x-www-form-urlencoded"}}).then(res=>{
+		return res.json();
+	})
+		.then(function(json) {
 		console.log(json)
-		let meslist=yqjson.data.area;
-		for(let key in json.features){                                                      let pro=json.features[key];                                                 item=meslist.find((mes)=>{                                                          return parseInt(mes.locationId)===parseInt(pro.properties.adcode);                                                                              })||{};
-                        pro.properties.confirmedCount=item.confirmedCount||0;                                                                                                   pro.properties.curedCount=item.curedCount||0;                               pro.properties.deadCount=item.deadCount||0;                                                                                                    }	
-		let vectorSource = new ol.source.Vector({
-			features: (new ol.format.GeoJSON()).readFeatures(json,{
-				featureProjection:'EPSG:3857'
- })
-		});
-		console.log((new ol.format.GeoJSON({dataProjection:"epsg:4326"})).readFeatures(json))
-
-		let style = new ol.style.Style({                                      fill: new ol.style.Fill({                                                     color: 'rgba(255, 100, 100, 0.6)'                                         }),                                                                         stroke: new ol.style.Stroke({                                                 color: '#319FD3',                                                           width: 1                                                                  }),                                                                         text: new ol.style.Text({                                                     font: '12px Calibri,sans-serif',                                            fill: new ol.style.Fill({                                                     color: '#000'
-    }),
-    stroke: new ol.style.Stroke({                                                 color: '#fff',                                                              width: 3                                                                  })                                                                        })
-});
-		let vectorLayer = new ol.layer.Vector({
-  source: vectorSource,
-                    style: function(feature) {                                  style.getText().setText(feature.get('name'));
-		let fillcolor='rgba(255,255,255,0.6)';
-		let level=0;
-		let confirmed=parseInt(feature.get('confirmedCount'));
-		if(confirmed>9)
-			    level++;
-		if(confirmed>99)
-			    level++;
-		if(confirmed>499)
-			    level++;
-		if(confirmed>999)
-			    level++;
-		if(confirmed>5999)
-			    level++;
-			    style.getFill().setColor('rgba(255,'+(255-level*50)+',0,0.6)');
-			    return style;                                                               }                                                            
-		});
-	map.addLayer(vectorLayer);
-	console.log(vectorLayer.getSource().getFeatures());
-	});
-
-})
+        getyiqing().then((yqjson) => {
+            console.log(yqjson)
+            console.log(json)
+            let meslist = yqjson.data.area;
+            for (let key in json.features) {
+                let pro = json.features[key];
+                item = meslist.find((mes) => {
+                    return parseInt(mes.locationId) === parseInt(pro.properties.adcode);
+                }) || {};
+                pro.properties.confirmedCount = item.confirmedCount || 0;
+                pro.properties.curedCount = item.curedCount || 0;
+                pro.properties.deadCount = item.deadCount || 0;
+            }
+            let vectorSource = new ol.source.Vector({
+                features: (new ol.format.GeoJSON()).readFeatures(json, {
+                    featureProjection: 'EPSG:3857'
+                })
+            });
+            console.log((new ol.format.GeoJSON({
+                dataProjection: "epsg:4326"
+            })).readFeatures(json))
+ 
+            let style = new ol.style.Style({
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 100, 100, 0.6)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: '#319FD3',
+                    width: 1
+                }),
+                text: new ol.style.Text({
+                    font: '12px Calibri,sans-serif',
+                    fill: new ol.style.Fill({
+                        color: '#000'
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: '#fff',
+                        width: 3
+                    })
+                })
+            });
+            let vectorLayer = new ol.layer.Vector({
+                source: vectorSource,
+                style: function(feature) {
+                    style.getText().setText(feature.get('name'));
+                    let fillcolor = 'rgba(255,255,255,0.6)';
+                    let level = 0;
+                    let confirmed = parseInt(feature.get('confirmedCount'));
+                    if (confirmed > 9)
+                        level++;
+                    if (confirmed > 99)
+                        level++;
+                    if (confirmed > 499)
+                        level++;
+                    if (confirmed > 999)
+                        level++;
+                    if (confirmed > 5999)
+                        level++;
+                    style.getFill().setColor('rgba(255,' + (255 - level * 50) + ',0,0.6)');
+                    return style;
+                }
+            });
+            map.addLayer(vectorLayer);
+            console.log(vectorLayer.getSource().getFeatures());
+        });
+ 
+    })
 
 let yqurl="https://tianqiapi.com/api?version=epidemic&appid=22747463&appsecret=6uKNYwJa"
 let yqurl1="https://interface.sina.cn/news/wap/fymap2020_data.d.json"
 function getyiqing(){
-return fetch(yqurl,{mode:"cors"}).then(function(res){
+	return fetch(yqurl,{mode:"cors"}).then(function(res){
 return res.json();
-});
+}).catch(err=>console.log(err));
 }
-
-
 }
 
 

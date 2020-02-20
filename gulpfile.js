2b86a2387ad5d093de5786ef9105de89f87f3cb2
retrun18 <648437254@qq.com>
2020-02-20 13:45:22 +0800

    globe

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,39 @@
-let {watch,src,dest}        = require('gulp');
+let {watch,src,dest,series}        = require('gulp');
 let browserSync = require('browser-sync').create();
+let terser=require('gulp-terser-js');
+let filter=require('gulp-filter');
+let changed=require('gulp-changed');
 
 function defaultTask(cb){
-console.log("This is default task!");
-cb()
+	console.log("This is default task!");
+	cb()
 }
 const debugWatcher=watch(["./source/*.html","./source/script/**.*","./source/style/**.*"]) 
 function debugServer(){
-    browserSync.init({
-        server:{basedir:"./source/",
-		index:"./source/map.html"
-	}
-    });
-    debugWatcher.on('change',function(path,stats){
-	src(path)
-	.pipe(browserSync.reload({stream:true}))
-    });
+	browserSync.init({
+		server:{baseDir:"source",
+			index:"globe.html"
+		}
+	});
+	debugWatcher.on('change',function(path,stats){
+		src(path)
+			.pipe(browserSync.reload({stream:true}))
+	});
+}
+const build=series(minifyjs);
+const jsf=filter(['source/script/**/*.js'],{restore:true});
+const builddest="build/";
+function minifyjs(cb){
+	src(['source/**/*'])
+	.pipe(changed(builddest))
+	.pipe(jsf)
+	.pipe(terser({mangle:{toplevel:false}}))
+	.pipe(jsf.restore)
+	.pipe(dest(builddest));
+	cb();
 }
 
 
 exports.debugserver=debugServer;
 exports.default=defaultTask;
+exports.build=build;
--- source/script/main.js
+++ source/script/main.js
@@ -29,99 +29,99 @@ function showInfo(event) {
   let features = map.getFeaturesAtPixel(event.pixel);
   if (features.length == 0) {
     info.innerText = '';
-    info.style.opacity = 0;
-    return;
+	  info.style.opacity = 0;
+	  return;
   }
-  var mes = features[0].getProperties();
-  info.innerText =mes.name+ "\n累计确诊:"+mes.confirmedCount+
+	var mes = features[0].getProperties();
+	info.innerText =mes.name+ "\n累计确诊:"+mes.confirmedCount+
 "\n当前确诊:"+mes.currentConfirmedCount+"\n治愈人数:"+mes.curedCount+
 "\n死亡人数:"+mes.deadCount+(mes.suspectedCount==0?"":"\n疑似病例:"+mes.suspectedCount);
-  info.style.opacity = .7;
+	info.style.opacity = .7;
 }
 
 let areasurl='./script/100000_full.json';
 let yqurl2="https://tianqiapi.com/api?version=epidemic&appid=22747463&appsecret=6uKNYwJa"
-	fetch(areasurl,/*{method:'get',mode:'cors',headers:{'Content-Type':"application/x-www-form-urlencoded"}}*/).then(res=>{
-		return res.json();
-	})
-		.then(function(json) {
+fetch(areasurl,/*{method:'get',mode:'cors',headers:{'Content-Type':"application/x-www-form-urlencoded"}}*/).then(res=>{
+	return res.json();
+})
+	.then(function(json) {
 		//console.log(json)
-        getyiqing().then((yqjson) => {
-            console.log(yqjson)
-	    alert("全国累计确诊:"+yqjson.data.diagnosed
-	    +";\n疑似病例:"+yqjson.data.suspect+";\n死亡人数:"
-	    +yqjson.data.death+";\n治愈人数:"+yqjson.data.cured
-	    +";\n重症病例:"+yqjson.data.serious+";");
-            //console.log(json)
-            let meslist = yqjson.data.area;
-            for (let key in json.features) {
-                let pro = json.features[key];
-                item = meslist.find((mes) => {
-                    return parseInt(mes.locationId) === parseInt(pro.properties.adcode);
-                }) || {};
-                pro.properties.confirmedCount = item.confirmedCount || 0;
-                pro.properties.curedCount = item.curedCount || 0;
-                pro.properties.deadCount = item.deadCount || 0;
-            	pro.properties.currentConfirmedCount=item.currentConfirmedCount ||0;
-		pro.properties.suspectedCount=item.suspectedCount||0;
-	    }
-            let vectorSource = new ol.source.Vector({
-                features: (new ol.format.GeoJSON()).readFeatures(json, {
-                    featureProjection: 'EPSG:3857'
-                })
-            }); 
-            let style = new ol.style.Style({
-                fill: new ol.style.Fill({
-                    color: 'rgba(255, 100, 100, 0.6)'
-                }),
-                stroke: new ol.style.Stroke({
-                    color: '#319FD3',
-                    width: 1
-                }),
-                text: new ol.style.Text({
-                    font: '12px Calibri,sans-serif',
-                    fill: new ol.style.Fill({
-                        color: '#000'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#fff',
-                        width: 3
-                    })
-                })
-            });
-            let vectorLayer = new ol.layer.Vector({
-                source: vectorSource,
-		    maxZoom:9,
-                style: function(feature) {
-                    style.getText().setText(feature.get('name'));
-                    let fillcolor = 'rgba(255,255,255,0.6)';
-                    let level = 0;
-                    let confirmed = parseInt(feature.get('confirmedCount'));
-                    if (confirmed > 9)
-                        level++;
-                    if (confirmed > 99)
-                        level++;
-                    if (confirmed > 499)
-                        level++;
-                    if (confirmed > 999)
-                        level++;
-                    if (confirmed > 5999)
-                        level++;
-                    style.getFill().setColor('rgba(255,' + (255 - level * 50) + ',0,0.6)');
-                    return style;
-                }
-            });
-            map.addLayer(vectorLayer);
-        });
- 
-    })
+		getyiqing().then((yqjson) => {
+			console.log(yqjson)
+			alert("全国累计确诊:"+yqjson.data.diagnosed
+				+";\n疑似病例:"+yqjson.data.suspect+";\n死亡人数:"
+				+yqjson.data.death+";\n治愈人数:"+yqjson.data.cured
+				+";\n重症病例:"+yqjson.data.serious+";");
+			//console.log(json)
+			let meslist = yqjson.data.area;
+			for (let key in json.features) {
+				let pro = json.features[key];
+				item = meslist.find((mes) => {
+					return parseInt(mes.locationId) === parseInt(pro.properties.adcode);
+				}) || {};
+				pro.properties.confirmedCount = item.confirmedCount || 0;
+				pro.properties.curedCount = item.curedCount || 0;
+				pro.properties.deadCount = item.deadCount || 0;
+				pro.properties.currentConfirmedCount=item.currentConfirmedCount ||0;
+				pro.properties.suspectedCount=item.suspectedCount||0;
+			}
+			let vectorSource = new ol.source.Vector({
+				features: (new ol.format.GeoJSON()).readFeatures(json, {
+					featureProjection: 'EPSG:3857'
+				})
+			}); 
+			let style = new ol.style.Style({
+				fill: new ol.style.Fill({
+					color: 'rgba(255, 100, 100, 0.6)'
+				}),
+				stroke: new ol.style.Stroke({
+					color: '#319FD3',
+					width: 1
+				}),
+				text: new ol.style.Text({
+					font: '12px Calibri,sans-serif',
+					fill: new ol.style.Fill({
+						color: '#000'
+					}),
+					stroke: new ol.style.Stroke({
+						color: '#fff',
+						width: 3
+					})
+				})
+			});
+			let vectorLayer = new ol.layer.Vector({
+				source: vectorSource,
+				maxZoom:9,
+				style: function(feature) {
+					style.getText().setText(feature.get('name'));
+					let fillcolor = 'rgba(255,255,255,0.6)';
+					let level = 0;
+					let confirmed = parseInt(feature.get('confirmedCount'));
+					if (confirmed > 9)
+						level++;
+					if (confirmed > 99)
+						level++;
+					if (confirmed > 499)
+						level++;
+					if (confirmed > 999)
+						level++;
+					if (confirmed > 5999)
+						level++;
+					style.getFill().setColor('rgba(255,' + (255 - level * 50) + ',0,0.6)');
+					return style;
+				}
+			});
+			map.addLayer(vectorLayer);
+		});
+
+	})
 
 let yqurl="https://tianqiapi.com/api?version=epidemic&appid=22747463&appsecret=6uKNYwJa"
 let yqurl1="https://interface.sina.cn/news/wap/fymap2020_data.d.json"
 function getyiqing(){
 	return fetch(yqurl,{mode:"cors"}).then(function(res){
-return res.json();
-}).catch(err=>console.log(err));
+		return res.json();
+	}).catch(err=>console.log(err));
 }
 }
 
